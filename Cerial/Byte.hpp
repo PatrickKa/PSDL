3a5f0e439eaaaac999df3a7e79dd7ec70eeb7ff2
Patrick Kappl <kappl.pa@gmail.com>
2026-02-09 22:11:43 +0100

    Add custom byte type with literal operator

--- Cerial/Byte.hpp
+++ Cerial/Byte.hpp
@@ -1,1 +1,16 @@
 #pragma once
+
+
+#include <cstddef>
+
+
+namespace cerial
+{
+using Byte = std::byte;
+
+
+constexpr auto operator""_b(unsigned long long number) -> Byte
+{
+    return static_cast<Byte>(number);
+}
+}
