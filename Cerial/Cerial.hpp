3c60a28340dae53bc53d49c5134f257ad2c0c639
Patrick Kappl <kappl.pa@gmail.com>
2026-02-28 16:03:11 +0100

    Add SerialSize<>() and TotalSerialSize<>()

--- Cerial/Cerial.hpp
+++ Cerial/Cerial.hpp
@@ -1,1 +1,87 @@
 #pragma once
+
+#include <array>
+#include <cstddef>
+#include <type_traits>
+
+
+namespace cerial
+{
+// --- Concepts and traits ---
+
+template<typename T>
+concept TriviallySerializable = std::is_arithmetic_v<T> || std::is_enum_v<T>;
+
+template<typename T>
+inline constexpr auto isStdArray = false;
+
+template<typename T, std::size_t size>
+inline constexpr auto isStdArray<std::array<T, size>> = true;
+
+template<typename T>
+concept StdArray = isStdArray<T>;
+
+
+// --- Function declarations ---
+
+// Must be specialized for user-defined types. The primary template is intentionally left undefined,
+// so that the compiler will throw an error if the specialization is missing.
+template<typename T>
+constexpr auto SerialSize() -> std::size_t;
+
+template<TriviallySerializable T>
+constexpr auto SerialSize() -> std::size_t;
+
+template<StdArray T>
+constexpr auto SerialSize() -> std::size_t;
+
+template<typename... Ts>
+constexpr auto TotalSerialSize() -> std::size_t;
+
+
+// --- Function definitions ---
+
+template<TriviallySerializable T>
+constexpr auto SerialSize() -> std::size_t
+{
+    return sizeof(T);
+}
+
+
+template<StdArray T>
+constexpr auto SerialSize() -> std::size_t
+{
+    return SerialSize<typename T::value_type>() * std::tuple_size_v<T>;
+}
+
+
+template<typename... Ts>
+constexpr auto TotalSerialSize() -> std::size_t
+{
+    return (SerialSize<Ts>() + ... + 0UZ);
+}
+
+
+// --- Compile time checks ---
+
+static_assert(TriviallySerializable<bool>);
+static_assert(TriviallySerializable<char>);
+static_assert(TriviallySerializable<int>);
+static_assert(TriviallySerializable<unsigned long long>);
+static_assert(TriviallySerializable<float>);
+static_assert(TriviallySerializable<double>);
+static_assert(!TriviallySerializable<void *>);
+
+static_assert(SerialSize<bool>() == 1);
+static_assert(SerialSize<char>() == 1);
+static_assert(SerialSize<int>() == sizeof(int));
+static_assert(SerialSize<unsigned long long>() == sizeof(unsigned long long));
+// static_assert(SerialSize<void *>() == 4);  // "Undefined function 'SerialSize<void *>' ..."
+
+static_assert(SerialSize<std::array<char, 2>>() == 2);
+static_assert(SerialSize<std::array<std::array<float, 2>, 3>>() == 4 * 3 * 2);
+
+static_assert(TotalSerialSize<>() == 0);
+static_assert(TotalSerialSize<float>() == 4);
+static_assert(TotalSerialSize<float, std::array<char, 3>>() == 4 + 3);
+}
